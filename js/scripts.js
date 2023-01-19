45bf43cbd7b0d8f5ccf96b7c1ee432593be35e8b
Daniel Jiménez <djimenezweb@gmail.com>
2023-01-19 18:55:18 +0100

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,118 @@
+const startButton = document.getElementById('start-button');
+const gameQuestionElement = document.getElementById('game-question');
+const answersElement = document.getElementById('answers');
+
+const QUESTIONS = {
+  programming: [
+    {
+      question: '¿Cuántos tipos de bucles hay en programación?',
+      answers: {
+        options: ['1', '2', '3', '50'],
+        correctAnswer: 1
+      },
+      theme: 'programming',
+      hasAnswered: false
+    },
+    {
+      question: '¿Cuántos lenguajes de programación entienden los navegadores?',
+      answers: {
+        options: ['3', '2', '1', '20'],
+        correctAnswer: 2
+      },
+      theme: 'programming',
+      hasAnswered: false
+    }
+  ],
+  math: [
+    {
+      question: '¿Cuánto es 5 x 8?',
+      answers: {
+        options: ['40', '25', '30', '50'],
+        correctAnswer: 0
+      },
+      theme: 'math',
+      hasAnswered: false
+    },
+    {
+      question: '¿Cuánto es la cuarta parte de la tercera parte?',
+      answers: {
+        options: ['Un doceavo', 'Un séptimo', '3/4 partes', '4/6 partes'],
+        correctAnswer: 0
+      },
+      theme: 'math',
+      hasAnswered: false
+    }
+  ],
+  science: [
+    {
+      question: '¿En qué dirección apunta la aguja de una brújula?',
+      answers: {
+        options: ['Sur', 'Este', 'Oeste', 'Norte'],
+        correctAnswer: 3
+      },
+      theme: 'science',
+      hasAnswered: false
+    },
+    {
+      question: '¿Qué mineral se encuentra en una solución salina?',
+      answers: {
+        options: ['Potasio', 'Calcio', 'Sodio', 'Magnesio'],
+        correctAnswer: 2
+      },
+      theme: 'science',
+      hasAnswered: false
+    }
+  ],
+  history: [
+    {
+      question: '¿En qué año piso el hombre la luna por primera vez?',
+      answers: {
+        options: ['1830', '1969', '2001', 'Fue un montaje'],
+        correctAnswer: 1
+      },
+      theme: 'history',
+      hasAnswered: false
+    },
+    {
+      question: '¿Dónde se inventó la pólvora?',
+      answers: {
+        options: ['China', 'Francia', 'EEUU', 'Italia'],
+        correctAnswer: 0
+      },
+      theme: 'history',
+      hasAnswered: false
+    }
+  ],
+  random: [
+    {
+      question: '¿Dónde originaron los juegos olímpicos?',
+      answers: {
+        options: ['Cuenca', 'Grecia', 'Alemania', 'Dublin'],
+        correctAnswer: 1
+      },
+      hasAnswered: false
+    },
+    {
+      question: '¿Cuántos huesos tiene un humano adulto?',
+      answers: {
+        options: ['412', '287', '306', '328'],
+        correctAnswer: 2
+      },
+      hasAnswered: false
+    },
+    {
+      question: '¿Quién fue el fundador de Kodak?',
+      answers: {
+        options: ['Auguste Lumière', 'Thomas Edison', 'George Eastman', 'Eadweard Muybridge'],
+        correctAnswer: 2
+      },
+      hasAnswered: false
+    }
+  ]
+};
+
+const randomNumber = number => {
+  return Math.floor(Math.random() * (number + 1));
+};
+
+startButton.addEventListener('click', () => {});
